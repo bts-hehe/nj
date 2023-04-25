d177f8dfb52d0076c524ffdf30a23ee37bd021c6
prince-of-tennis <86171033+prince-of-tennis@users.noreply.github.com>
2023-04-24 19:59:45 -0700

    firewall rules final commit for today

--- crunch.ps1
+++ crunch.ps1
@@ -1,12 +1,17 @@
 # start firewall, fix it maybe
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
-Set-NetFirewallProfile -DefaultInboundAction Block -DefaultOutboundAction Block –NotifyOnListen True
+Set-NetFirewallProfile -DefaultInboundAction Block -DefaultOutboundAction Block
 
-<#
-multi-line comments look like this
-#>
+# firewall rules
+Disable-NetFirewallRule -All
 New-NetFirewallRule -DisplayName “loi” -Direction Inbound -RemoteAddress LocalSubnet -Action Allow # don't know if these 2 rules work
 New-NetFirewallRule -DisplayName “loo" -Direction Outbound -RemoteAddress LocalSubnet -Action Allow
+
+$ServicePorts = Read-Host "Hosted checked service ports:"
+New-NetFirewallRule -DisplayName “si” -Direction Inbound -RemotePort $ServicePorts -Action Allow 
+
+
+
 
 # disable/enable services
 set-service eventlog -start a -status running
