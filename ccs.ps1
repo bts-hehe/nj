b6264844871603ff78905bb581c34b34b39acf19
prince-of-tennis <86171033+prince-of-tennis@users.noreply.github.com>
2023-04-25 16:58:08 -0700

    Create ccs.ps1

---
+++ ccs.ps1
@@ -0,0 +1,44 @@
+# start firewall, fix it maybe
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+Set-NetFirewallProfile -DefaultInboundAction Block -DefaultOutboundAction Block
+
+# firewall rules
+Disable-NetFirewallRule -All
+New-NetFirewallRule -DisplayName “local in” -Direction Inbound -RemoteAddress LocalSubnet -Action Allow # don't know if these 2 rules work
+New-NetFirewallRule -DisplayName “local out" -Direction Outbound -RemoteAddress LocalSubnet -Action Allow
+
+$ServicePorts = Read-Host "Hosted checked service ports:"
+New-NetFirewallRule -DisplayName “service in” -Direction Inbound -LocalPort $ServicePorts -Action Allow 
+
+# disable guest/Administrator accounts
+Disable-LocalUser -Name "Administrator"
+Disable-LocalUser -Name "Guest"
+
+Disable-ADAccount -Name "Administrator"
+Disable-ADAccount -Name "Guest"
+
+# Defender
+start-service WinDefend
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "ServiceKeepAlive" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Scan" /v "DisableHeuristics" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Attachments" /v "ScanWithAntiVirus" /t REG_DWORD /d 3 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Scan" /v "CheckForSignaturesBeforeRunningScan" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Reporting" /v "DisableGenericRePorts" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "LocalSettingOverrideSpynetReporting" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "SubmitSamplesConsent" /t REG_DWORD /d 2 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "DisableBlockAtFirstSeen" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "SpynetReporting" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Features" /v TamperProtection /t REG_DWORD /d 5 /F
+
+# disable/enable services
+set-service eventlog -start a -status running
+set-service snmptrap -start d -status stopped
+set-service iphlpsvc -start d -status stopped
+
+# passwords
+Get-LocalUser | Set-LocalUser -Password (Read-Host -AsSecureString "local pass: ")
+Get-ADUser | Set-ADUser -Password (Read-Host -AsSecureString "AD pass:")
