ae214624fb8d954875028758ae977aadfc3ab93c
prince-of-tennis <86171033+prince-of-tennis@users.noreply.github.com>
2023-04-25 17:00:19 -0700

    rename files for clarity

--- ccs.ps1
+++ ccs.ps1
@@ -1,14 +1,6 @@
 # start firewall, fix it maybe
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
-Set-NetFirewallProfile -DefaultInboundAction Block -DefaultOutboundAction Block
-
-# firewall rules
-Disable-NetFirewallRule -All
-New-NetFirewallRule -DisplayName “local in” -Direction Inbound -RemoteAddress LocalSubnet -Action Allow # don't know if these 2 rules work
-New-NetFirewallRule -DisplayName “local out" -Direction Outbound -RemoteAddress LocalSubnet -Action Allow
-
-$ServicePorts = Read-Host "Hosted checked service ports:"
-New-NetFirewallRule -DisplayName “service in” -Direction Inbound -LocalPort $ServicePorts -Action Allow 
+Set-NetFirewallProfile -DefaultInboundAction Block -DefaultOutboundAction Allow
 
 # disable guest/Administrator accounts
 Disable-LocalUser -Name "Administrator"
