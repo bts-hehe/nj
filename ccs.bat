66e8049d5faf12a1a451662d600c79bf695da436
Tai-Yu Chen <86171033+prince-of-tennis@users.noreply.github.com>
2023-06-15 19:41:10 -0700

    users

--- ccs.bat
+++ ccs.bat
@@ -4,8 +4,10 @@ netsh advfirewall set allprofiles state on
 
 :: users
 set password="CyberPatriot123!@#"
-for /F "tokens=*" %%A in (C:\temp\users.txt) do (
-  net user /domain %%A %password%
+for /F "tokens=*" %%user in (C:\temp\users.txt) do (
+  :: net user /domain %%user %password%
+  :: net user /domain %%user /ACTIVE NO
+  echo %%user
 )
 
 echo "|| finishing script ||"
