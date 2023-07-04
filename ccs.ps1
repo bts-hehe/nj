226e4a4ededbe109e674ff756823a39d5e372c2f
Tai-Yu Chen <86171033+prince-of-tennis@users.noreply.github.com>
2023-07-04 00:07:34 -0700

    () are not required for functions if they don't use parameters

--- ccs.ps1
+++ ccs.ps1
@@ -20,7 +20,7 @@ Stop-Transcript
 Invoke-Item ".\log.txt"
 
 # functions
-function Enable-Firewall(){
+function Enable-Firewall {
     Set-Service mpssvc -StartupType Automatic
     Start-Service mpssvc
     Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
@@ -52,18 +52,18 @@ function Set-Users([string]$Password){
         }
     }       
 }
-function Import-GPO(){
+function Import-GPO{
     Foreach ($gpoitem in Get-ChildItem ".\GPOs") {
         $gpopath = ".\GPOs\$gpoitem"
         LGPO.exe /g $gpopath > $null 2>&1
     }
     gpdupdate /force
 }
-function Import-Secpol(){
+function Import-Secpol {
     $dir ='.\secpol.inf'
     secedit.exe /configure /db %windir%\security\local.sdb /cfg $dir
 }
-function Disable-Services(){
+function Disable-Services {
     foreach($line in [System.IO.File]::ReadLines("enabled_services.txt"))
     {
         Set-Service $line -StartupType Automatic
@@ -75,16 +75,16 @@ function Disable-Services(){
         Stop-Service $line
     }
 }
-function Disable-Features(){
+function Disable-Features {
     Disable-PSRemoting -Force
     Disable-WindowsOptionalFeature -Online -FeatureName TelnetClient
     Disable-WindowsOptionalFeature -Online -FeatureName TelnetServer
     Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
 }
-function Set-Browser-Settings(){
+function Set-Browser-Settings {
     # should eventually include chrome, edge, internet explorer, firefox and some form of parameter to toggle each of these
 }
-function Set-UAC(){
+function Set-UAC {
     reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /t REG_DWORD /v FilterAdministratorToken /d 1 /f
     reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /t REG_DWORD /v EnableUIADesktopToggle /d 0 /f
     reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /t REG_DWORD /v ConsentPromptBehaviorUser /d 0 /f 
@@ -93,7 +93,7 @@ function Set-UAC(){
     reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /t REG_DWORD /v EnableLUA /d 1 /f 
     reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /t REG_DWORD /v EnableVirtualization /d 0 /f 
 }
-function Set-Audit-Policy(){
+function Set-Audit-Policy {
     auditpol /set /category:"Account Logon" /success:enable /failure:enable
     auditpol /set /category:"Account Management" /success:enable /failure:enable
     auditpol /set /category:"Detailed Tracking" /success:enable /failure:enable
@@ -104,7 +104,7 @@ function Set-Audit-Policy(){
     auditpol /set /category:"Privilege Use" /success:enable /failure:enable
     auditpol /set /category:"System" /success:enable /failure:enable
 }
-function Enable-Windows-Defender(){
+function Enable-Windows-Defender {
     Remove-MpPreference -ExclusionPath ( Get-MpPreference | Select-Object -Property ExclusionPath -ExpandProperty ExclusionPath)
     reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d 0 /f
     reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d 0 /f
@@ -121,24 +121,24 @@ function Enable-Windows-Defender(){
     reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "SpynetReporting" /t REG_DWORD /d 0 /f
     reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Features" /v TamperProtection /t REG_DWORD /d 5 /F
 }
+function Set-Misc-Settings {
+    net share C:\ /delete
+    bcdedit /set {current} nx AlwaysOn
 
-# ---MORE HARDENING---
-net share C:\ /delete
-bcdedit /set {current} nx AlwaysOn
+    reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile /v EnableFirewall /t REG_DWORD /d 1 /f # enable firewall
+    reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System /t REG_DWORD /v EnableSmartScreen /d 1 /f # enable smartscreen, this is a problem if downloading software
+    reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers  /v DisableAutoplay /t REG_DWORD /d 1 /f # disable autoplay
+    reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 0 /f # enable autoupdate
+    reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 3 /f # enable autoupdate
+    reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Remote Assistance" /v fAllowToGetHelp /t REG_DWORD /d 0 /f # user cannot request remote assistance
+    reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /t REG_DWORD /v fDenyTSConnections /d 0 /f # disable remote desktop
+    reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server"/t REG_DWORD /v fSingleSessionPerUser /d 1 /f # disable auto admin login
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Autoadminlogin /t REG_SZ /d 0 /f # disable auto admin login
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DisableCAD /t REG_DWORD /d 1 /f # disable ctrl-alt-delete to login
+    reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest" /v UseLogonCredential /t REG_DWORD /d 0 /f # disable WDigest
+    reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 1 /f # enable RDP NLA (network level auth)
+    reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NTDS\Parameters" /v LdapEnforceChannelBinding /t REG_DWORD /d 1 /f # make LDAP authentication over SSL/TLS more secure
+    reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DNS\Parameters" /v TcpReceivePacketSize /t REG_DWORD /d 0xFF00 # https://support.microsoft.com/en-au/topic/kb4569509-guidance-for-dns-server-vulnerability-cve-2020-1350-6bdf3ae7-1961-2d25-7244-cce61b056569
 
-reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile /v EnableFirewall /t REG_DWORD /d 1 /f # enable firewall
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System /t REG_DWORD /v EnableSmartScreen /d 1 /f # enable smartscreen, this is a problem if downloading software
-reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers  /v DisableAutoplay /t REG_DWORD /d 1 /f # disable autoplay
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 0 /f # enable autoupdate
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 3 /f # enable autoupdate
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Remote Assistance" /v fAllowToGetHelp /t REG_DWORD /d 0 /f # user cannot request remote assistance
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /t REG_DWORD /v fDenyTSConnections /d 0 /f # disable remote desktop
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server"/t REG_DWORD /v fSingleSessionPerUser /d 1 /f # disable auto admin login
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Autoadminlogin /t REG_SZ /d 0 /f # disable auto admin login
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DisableCAD /t REG_DWORD /d 1 /f # disable ctrl-alt-delete to login
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest" /v UseLogonCredential /t REG_DWORD /d 0 /f # disable WDigest
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 1 /f # enable RDP NLA (network level auth)
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NTDS\Parameters" /v LdapEnforceChannelBinding /t REG_DWORD /d 1 /f # make LDAP authentication over SSL/TLS more secure
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DNS\Parameters" /v TcpReceivePacketSize /t REG_DWORD /d 0xFF00 # https://support.microsoft.com/en-au/topic/kb4569509-guidance-for-dns-server-vulnerability-cve-2020-1350-6bdf3ae7-1961-2d25-7244-cce61b056569
-
-# HKLM\System\CurrentControlSet\Services\DNS\Parameters\SecureResponses
+    # HKLM\System\CurrentControlSet\Services\DNS\Parameters\SecureResponses
+}
