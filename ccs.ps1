dc083e060f2683cd6635cabe086a3f1b35b61e27
Tai-Yu Chen <86171033+prince-of-tennis@users.noreply.github.com>
2023-07-06 20:15:02 -0700

    a more accurate service status message

--- ccs.ps1
+++ ccs.ps1
@@ -61,14 +61,14 @@ function Set-Services {
     $EnabledServices = Get-Content -Path "enabled_services.txt"
     $DisabledServices = Get-Content -Path "disabled_services.txt"
     foreach($Service in $EnabledServices) {
+        Write-Output "Starting $Service"
         Set-Service $Service -StartupType Automatic
         Start-Service $Service
-        Write-Output "Started $Service"
     }
     foreach($Service in $DisabledServices) {
+        Write-Output "Stopping $Service"
         Set-Service $Service -StartupType Disabled
         Stop-Service $Service
-        Write-Output "Stopped $Service"
     }
 }
 function Set-Features {
