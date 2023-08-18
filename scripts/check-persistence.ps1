cdfb17f640c1d25ad9e4584add89221f2809ddcc
Tai-Yu Chen <86171033+prince-of-tennis@users.noreply.github.com>
2023-08-17 19:07:55 -0700

    - semicolons are comments in .inf files, added comments to secpol.inf - no idea how the registry keys of secpol.inf work -> the 4,2 etc, what is the 4 for? - kinda worked out how to save output of reg query -> now how to parse?

--- scripts/check-persistence.ps1
+++ scripts/check-persistence.ps1
@@ -1,5 +1,7 @@
 Write-Output "Checking persistence methods"
 
+$run = (reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run")
+Write-Output $run
 #https://tech-zealots.com/malware-analysis/malware-persistence-mechanisms/
 
 # todo -> do a lot of reg queries
