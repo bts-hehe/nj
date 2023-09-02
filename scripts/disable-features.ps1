194030dc8c074acf5c91bf7644ea3d1cd7bbd699
utahorange <86171033+utahorange@users.noreply.github.com>
2023-09-02 00:20:44 -0700

    more comments about what to do for disable-features.ps1

--- scripts/disable-features.ps1
+++ scripts/disable-features.ps1
@@ -1,4 +1,10 @@
 Write-Output "`n---Disabling Insecure Windows Features"
+<#
+if(is server)
+    get-windowsfeatures
+    see which ones are good
+    disable bad ones
+#>
 Disable-PSRemoting -Force
 Disable-WindowsOptionalFeature -Online -FeatureName TelnetClient
 Disable-WindowsOptionalFeature -Online -FeatureName TelnetServer
