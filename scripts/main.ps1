a46d096a42fa92457c5d738f57df63555e5f7e2c
utahorange <86171033+utahorange@users.noreply.github.com>
2023-09-07 21:53:01 -0700

    proper way to run another script with PSScriptRoot is with & prepended

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,22 +1,22 @@
 Start-Transcript -Append ../logs/log.txt
 Write-Output "|| Welcome to Win ||`n"
 
-# $PSScriptRoot/fix.ps1
-$PSScriptRoot/install-tools.ps1
+#& $PSScriptRoot/fix.ps1
+& $PSScriptRoot/install-tools.ps1
 
-$PSScriptRoot/enable-firewall.ps1
-$PSScriptRoot/enable-defender.ps1
+& $PSScriptRoot/enable-firewall.ps1
+& $PSScriptRoot/enable-defender.ps1
 
-$SecurePassword = ConvertTo-SecureString -String 'CyberPatriot123!@#' -AsPlainText -Force
-$PSScriptRoot/configure-local-users.ps1 -Password $SecurePassword
-$PSScriptRoot/configure-ad-users.ps1 -Password $SecurePassword
+$SecurePassword = ConvertTo-SecureString -String 'MonkePatriot123!@#' -AsPlainText -Force
+& $PSScriptRoot/configure-local-users.ps1 -Password $SecurePassword
+& $PSScriptRoot/configure-ad-users.ps1 -Password $SecurePassword
 
-$PSScriptRoot/import-gpo.ps1
-$PSScriptRoot/import-secpol.ps1 # if secpol breaks, run uac.ps1, auditpol.ps1
+& $PSScriptRoot/import-gpo.ps1
+& $PSScriptRoot/import-secpol.ps1 # if secpol breaks, run uac.ps1, auditpol.ps1
 
-$PSScriptRoot/configure-services.ps1 -keepRD $false
+& $PSScriptRoot/configure-services.ps1 -keepRD $false
 
-$PSScriptRoot/remove-nondefaultshares.ps1 
+& $PSScriptRoot/remove-nondefaultshares.ps1 
 bcdedit /set {current} nx AlwaysOn
 
 Write-Output "|| ccs.ps1 finished ||`n"
