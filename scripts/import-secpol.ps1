9d6ba48c9de2a1b7a968e501b9fc05d76f49727a
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2023-10-12 16:43:30 -0700

    Update import-secpol.ps1
    haha i take that back my bad

--- scripts/import-secpol.ps1
+++ scripts/import-secpol.ps1
@@ -1,4 +1,4 @@
 Write-Output "`n---Configuring Local Security Policy"
-$dir ='..\win-main\secpol\secpol.inf' #annoying_secpol.inf
+$dir ='..\secpol\secpol.inf' #annoying_secpol.inf
 Write-Output "Importing Security Policy at $dir" 
 secedit.exe /configure /db C:\Windows\security\local.sdb /cfg $dir
