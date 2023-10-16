410d4410b413d075dbd274fbf49029e1e80416a5
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2023-10-16 16:31:12 -0700

    Update ad-users.ps1
    relative file path thing #2

--- scripts/ad-users.ps1
+++ scripts/ad-users.ps1
@@ -8,8 +8,8 @@ Disable-ADAccount -Name "Administrator"
 Disable-ADAccount -Name "Guest"
 Disable-ADAccount -Name "DefaultAccount"
 
-$Users = Get-Content -Path "users.txt" # list of authorized AD users from readme
-$Admins = Get-Content -Path "admins.txt" # list of authorized AD admins from readme
+$Users = Get-Content -Path "../users.txt" # list of authorized AD users from readme
+$Admins = Get-Content -Path "../admins.txt" # list of authorized AD admins from readme
 # at this point, assumes configure-local-users has already been run, bc it needs users.txt to also contain admins.txt list
 
 $DomainUsersOnImage = Get-ADAccount | Select-Object -ExpandProperty name
