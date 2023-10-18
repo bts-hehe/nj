455091c3468164d8d2058c3e6e4f6b295a888511
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2023-10-17 17:03:05 -0700

    Update import-gpo.ps1
    relative file path thing #3

--- scripts/import-gpo.ps1
+++ scripts/import-gpo.ps1
@@ -3,7 +3,7 @@ $gpresultOutput = (gpresult /r)
 $gpresultOutput | Set-Content -Path "../logs/gpresult.txt"
 # ^somehow parse this properly
 
-Foreach ($gpoitem in Get-ChildItem ".\GPOs") {
+Foreach ($gpoitem in Get-ChildItem ".\../GPOs") {
     Write-Output "Importing Group Policy $gpoitem"
     -cmd /c LGPO.exe /g ../GPOs
 }
