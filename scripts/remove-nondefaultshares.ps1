67268e594f089e7d7b38c99b573f1cd46342d755
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2023-10-22 15:04:24 -0700

    Update remove-nondefaultshares.ps1
    enabled dependent service

--- scripts/remove-nondefaultshares.ps1
+++ scripts/remove-nondefaultshares.ps1
@@ -1,4 +1,8 @@
 Write-Output "`n---Removing every share besides ADMIN$, C$, and IPC$"
+
+Set-Service LanmanServer -startuptype Automatic 
+Start-Service LanmanServer
+
 $Shares = Get-SmbShare | Select-Object -ExpandProperty name
 foreach($Share in $Shares) {
     if(($Share -ne "ADMIN$") -and ($Share -ne "C$") -and ($Share -ne "IPC$") -and ($Share -ne "SYSVOL") -and ($Share -ne "NETLOGON")) {
