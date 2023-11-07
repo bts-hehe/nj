e6a60c2733a952d65f1d401a5bd09fc1d93e7856
utahorange <86171033+utahorange@users.noreply.github.com>
2023-11-06 16:36:02 -0800

    Update registry-hardening.ps1
    i think the spaces at the end break sometimes, so requires ""

--- scripts/registry-hardening.ps1
+++ scripts/registry-hardening.ps1
@@ -1,3 +1,3 @@
 # a lot of registry keys in case secpol.msc doesn't work -> see secpol.msc and aperture windows vulns
 reg add HKLM\SYSTEM\CurrentControlSet\control\CrashControl /t REG_DWORD /v CrashDumpEnabled /d 0
-reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168 /t REG_DWORD /v Enabled /d 0
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168" /t REG_DWORD /v Enabled /d 0
