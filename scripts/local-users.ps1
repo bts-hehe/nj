953c590b55e7900fc9565e31ba1959f9cf44fcad
utahorange <86171033+utahorange@users.noreply.github.com>
2023-11-17 19:17:40 -0800

    Update local-users.ps1

--- scripts/local-users.ps1
+++ scripts/local-users.ps1
@@ -21,7 +21,6 @@ foreach($User in $Users) {
         New-LocalUser -Name $User -Password $Password
     }
 }
-Get-LocalUser | Set-LocalUser -Password $Password -PasswordNeverExpires $false # set everyone's password
 foreach($User in $UsersOnImage) {
     if ($Users -contains $User){ # if user is authorized
         Enable-LocalUser $User
@@ -37,3 +36,4 @@ foreach($User in $UsersOnImage) {
         Remove-LocalGroupMember -Group "Administrators" -Member $User
     }
 }
+Get-LocalUser | Set-LocalUser -Password $Password -PasswordNeverExpires $false # set everyone's password
