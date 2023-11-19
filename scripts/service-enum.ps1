6d9f2aa5c45478997f20ecc1d45d24a89d1447ba
utahorange <86171033+utahorange@users.noreply.github.com>
2023-11-19 10:35:40 -0800

    Update service-enum.ps1

--- scripts/service-enum.ps1
+++ scripts/service-enum.ps1
@@ -10,5 +10,10 @@ foreach($line in $services){
 if($productType -eq 1){
     Write-Output (compare-object (get-content $PSScriptRoot/baseline/services.txt) (get-content $PSScriptRoot/baseline/win10-default-services.txt))
 } else {
-    Write-Output (compare-object (get-content $PSScriptRoot/baseline/services.txt) (get-content $PSScriptRoot/baseline/server22-default-services.txt))
+    $serverVersion = Read-Host "Server 19 or 22?: [19/22] (Default: 22)"
+    if(($serverVersion -eq "19")){
+        Write-Output (compare-object (get-content $PSScriptRoot/baseline/services.txt) (get-content $PSScriptRoot/baseline/server19-default-services.txt))
+    } else {
+        Write-Output (compare-object (get-content $PSScriptRoot/baseline/services.txt) (get-content $PSScriptRoot/baseline/server22-default-services.txt))
+    }
 }
