03284254f8217ffc16d537fced99171cd69f6f17
utahorange <86171033+utahorange@users.noreply.github.com>
2023-11-21 22:13:42 -0800

    fixing ad users

--- scripts/ad-users.ps1
+++ scripts/ad-users.ps1
@@ -4,7 +4,6 @@ param (
 )
 Write-Output "`n---Configuring AD Users"
 
-
 $DomainUsers = Get-Content -Path "$PSScriptRoot/../users.txt" # list of authorized AD users from readme
 $DomainAdmins = Get-Content -Path "$PSScriptRoot/../admins.txt" # list of authorized AD admins from readme
 
@@ -14,14 +13,14 @@ Set-Content -Path "$PSScriptRoot/../logs/initial-ad-users.txt" $DomainUsersOnIma
 foreach($DomainUser in $DomainUsers) {
     if ($DomainUsersOnImage -notcontains $DomainUser){ # if user doesn't exist
         Write-Output "Adding Domain User $DomainUser"
-        New-ADUser -Name $DomainUser -AllowReversiblePasswordEncryption $false -PasswordNeverExpires $false
+        New-ADUser -Name $DomainUser -AccountPassword $Password -AllowReversiblePasswordEncryption $false -PasswordNeverExpires $false
     } 
 }
 
 foreach($DomainUser in $DomainAdmins) {
     if ($DomainUsersOnImage -notcontains $DomainUser){ # if user doesn't exist
         Write-Output "Adding Domain Admin $DomainUser"
-        New-ADUser -Name $DomainUser -AllowReversiblePasswordEncryption $false -PasswordNeverExpires $false
+        New-ADUser -Name $DomainUser -AccountPassword $Password -AllowReversiblePasswordEncryption $false -PasswordNeverExpires $false
     } 
 }
 
@@ -30,7 +29,7 @@ $DomainUsersOnImage = Get-ADUser -Filter * | Select-Object -ExpandProperty name 
 foreach($DomainUser in $DomainUsersOnImage) {
     if (!($DomainUsers -contains $DomainUser) -and !($DomainAdmins -contains $DomainUser)){
         Write-Output "Disabling user $DomainUser"
-        Disable-ADAccount $DomainUser
+        Disable-ADAccount -Identity $DomainUser
     } else {
         Write-Output "Enabling user $DomainUser"
         Enable-ADAccount -Identity $DomainUser
@@ -41,11 +40,11 @@ $AdminsOnImage = (Get-ADGroupMember -Identity "Domain Admins").name
 foreach($DomainUser in $DomainUsersOnImage) {
     if ($DomainAdmins -contains $DomainUser){ # if user is authorized domain admin because username was found in admins.txt 
         if(!($AdminsOnImage -contains ($DomainUser))){ # if user is auth admin and is not already added
-            Write-Output "Adding $DomainUser to Administrators Group"
+            Write-Output "Adding $DomainUser to Domain Admins group"
             Add-ADGroupMember -Identity "Domain Admins" -Members $DomainUser
         }
     } elseif(($AdminsOnImage -contains ($DomainUser)) -and ($DomainUser -ne 'Administrator')) { # if user is unauthorized, in admin group, and is not 'Administrator'
-        Write-Output "Removing $DomainUser from admin" 
+        Write-Output "Removing $DomainUser from Domain Admins group" 
         Remove-ADGroupMember -Identity "Domain Admins" -Members $DomainUser
     }
 }
