9bad7c792c0953545d37384531674f189161bdd9
utahorange <86171033+utahorange@users.noreply.github.com>
2024-01-07 19:30:07 -0800

    more asr rules

--- scripts/enable-defender.ps1
+++ scripts/enable-defender.ps1
@@ -26,10 +26,19 @@ reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Threats\ThreatSeverit
 # ASR rules 
 reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules" /v "3B576869-A4EC-4529-8536-B80A7769E899" /t REG_SZ /d 1 /f # V-213458	- block office apps from creating executable content
 reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules" /v "75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84" /t REG_SZ /d 1 /f # V-213459	- block office applications from injecting into other processes
-
 reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules" /v "BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550" /t REG_SZ /d 1 /f # block executable content from email client and webmail 
 reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules" /v "D4F940AB-401B-4EFC-AADC-AD5F3C50688A" /t REG_SZ /d 1 /f # block Office applications from creating child processes. 
-#reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules" /v "" /t REG_SZ /d 1 /f #
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules" /v "7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c" /t REG_SZ /d 1 /f # Block Adobe Reader from creating child processes
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v '9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2' /t REG_SZ /d 1 /f # Block credential stealing from the Windows local security authority subsystem (lsass.exe) 
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v '01443614-cd74-433a-b99e-2ecdc07bfc25' /t REG_SZ /d 1 /f # Block executable files from running unless they meet a prevalence, age, or trusted list criteria 
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v '5BEB7EFE-FD9A-4556-801D-275E5FFC04CC' /t REG_SZ /d 1 /f # Block execution of potentially obfuscated scripts 
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v 'D3E037E1-3EB8-44C8-A917-57927947596D' /t REG_SZ /d 1 /f # Block JavaScript or VBScript from launching downloaded executable content 
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v '26190899-1602-49e8-8b27-eb1d0a1ce869' /t REG_SZ /d 1 /f # Block Office communication applications from creating child processes 
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v 'e6db77e5-3df2-4cf1-b95a-636979351e5b' /t REG_SZ /d 1 /f # Block persistence through WMI event subscription 
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v 'd1e49aac-8f56-4280-b9ba-993a6d77406c' /t REG_SZ /d 1 /f # Block process creations originating from PSExec and WMI commands 
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v 'b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4' /t REG_SZ /d 1 /f # Block untrusted and unsigned processes that run from USB 
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v '92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B' /t REG_SZ /d 1 /f # Block Win32 API calls from Office macro 
+reg add 'HKLM\Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\ASR\Rules' /v 'c1db55ab-c21a-4637-bb3f-a12568109d35' /t REG_SZ /d 1 /f # Use advanced protection against ransomware 
 
 Set-MpPreference -EnableNetworkProtection Enabled
 Set-MpPreference -AllowNetworkProtectionOnWinServer $true
@@ -41,8 +50,6 @@ Set-MPPreference -DisableEmailScanning $false
 Set-MPPreference -DisableScriptScanning $false
 Set-MPPreference -DisableRealtimeMonitoring $false
 Set-MPPreference -DisableTamperProtection $false
-
-
 
 
 
