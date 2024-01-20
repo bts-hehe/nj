26e2e1b0fe740a401f83812cf8524e40e9604462
Swing-Rock <85258557+Swing-Rock@users.noreply.github.com>
2024-01-19 21:35:16 -0800

    Update task-stuff.ps1

--- scripts/task-stuff.ps1
+++ scripts/task-stuff.ps1
@@ -1,7 +1,20 @@
+param (
+    [Parameter(Mandatory)]
+    [int] $ProductType
+)
+
+if(($ProductType -eq "1") -or ($ProductType -eq "3")){
+    $defaultTasks = Get-Content -Path "$PSScriptRoot/../win10 tasks.txt" #replace with relative path name 
+    Write-Output "comparing against win10 tasks" 
+}else{
+    $defaultTasks = Get-Content -Path "$PSScriptRoot/../srv22 tasks.txt"
+    Write-Output "comparing against srv22 tasks" 
+}
+
 $tasks = Get-ScheduledTask | Select-Object -ExpandProperty TaskName 
-$srvTasks = Get-Content -Path "$PSScriptRoot/../tasks.txt" #replace with relative path name 
+
 foreach($task in $tasks){
-    if ($srvTasks -notcontains $task){
+    if ($defaultTasks -notcontains $task){
         Add-Content -Path "$PSScriptRoot/../extraTasks.txt" $task #replace with relative path 
     } 
 }
